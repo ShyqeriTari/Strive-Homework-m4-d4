542c09a70220a9d61b4d63710cea077a89f9724f
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-10 13:00:48 +0100

    Starting point

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,11 @@
+
+const MyFooter = () => (
+    
+   <footer className="d-flex bg-dark fixed-bottom">
+      <h5 className="text-light m-auto p-3">Strive Books - Copyright 2022 </h5>
+    </footer>
+)
+    
+
+
+export default MyFooter
